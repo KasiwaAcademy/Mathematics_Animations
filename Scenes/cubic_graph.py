87a64f71522cd4c238d67bc16ad8ce365716badd
McBrown Mwale <mcbrownwilfred@gmail.com>
2025-08-25 16:22:06 +0200

    Modify .gitignore and cubic_graph.py

--- Scenes/cubic_graph.py
+++ Scenes/cubic_graph.py
@@ -8,11 +8,48 @@ config.disable_caching = True
 
 class CubicGraph(Scene):
     def construct(self):
+        # Create a custom Latex template that includes the cancel package
+        my_template = TexTemplate()
+        my_template.add_to_preamble(r"\usepackage{cancel}")
 
-        # Title
-        title = Title(r"Welcome to Kasiwa academy")
+        # Load and position logo image
+        logo = ImageMobject("../Images/logo.png")
+        logo_corner = logo.scale(0.15)
+        logo_corner.to_corner(DR, buff=-0.2)
+        self.add(logo_corner)
+
+        # Intro
+        title = Tex(r"Solving Simultaneous Equations Graphically.", color=YELLOW)
+        institution = Tex(r"@Kasiwa Academy")
         self.play(Write(title))
-        self.wait(5)
+        self.wait()
+        self.play(title.animate.shift(UP).scale(1.3).set_color(YELLOW_B), FadeIn(institution, shift=UP))
+        self.wait(2)
+
+        # Problem Statement
+        sub_title_1 = Tex(r"Problem Statement:", color=YELLOW).to_edge(DOWN)
+        statement_1 = Tex(r"Solve the following simultaneous equations graphically:")
+        eq_group_1 = VGroup(
+            MathTex(r"y = x^3 + 3x^2 + x -2"),
+            MathTex(r"y = 2x + 1")
+        ).arrange(DOWN, buff=0.3, aligned_edge=LEFT).next_to(statement_1, DOWN, buff=0.3)
+        self.play(Write(sub_title_1))
+        self.wait()
+        self.play(FadeOut(title, institution, shift=UP), sub_title_1.animate.to_edge(UP).scale(1.3).set_color(WHITE), 
+                  FadeIn(statement_1, eq_group_1, shift=UP))
+        self.wait()
+        self.play(statement_1.animate.scale(1.1))
+        self.wait(2)
+
+        #Outro
+        final_text = Tex("Thank you for watching!", color=YELLOW_B)
+        self.play(Write(final_text), FadeOut(statement_1, sub_title_1, eq_group_1, shift=RIGHT))
+        self.wait()
+        self.play(logo_corner.animate.move_to(ORIGIN).scale(3), 
+                    final_text.animate.to_edge(DOWN).set_color(WHITE).scale(1.3))
+        self.wait()
+        self.play(FadeOut(final_text, logo_corner))
+        self.wait()
 
 # Thumbnail for CubicGraph
 class Thumbnail(Scene):
